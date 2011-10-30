e984d9383e546972ab165aaa1fb54b140e85832d
JoeBuntu <jcooper@joepserver.net>
2011-10-30 00:44:15 -0400

    Material - renamed part name to part number

--- Inventory/Inventory.Core/Entities/Material.cs
+++ Inventory/Inventory.Core/Entities/Material.cs
@@ -7,7 +7,7 @@ namespace Inventory.Core.Entities
 {
     public class Material : EntityBase
     {
-        public virtual string PartName { get; set; }
+        public virtual string PartNumber { get; set; }
         public virtual string Description { get; set; }
         public virtual MaterialType Type { get; set; }
         public virtual decimal PiecesPerCase { get; set; }
