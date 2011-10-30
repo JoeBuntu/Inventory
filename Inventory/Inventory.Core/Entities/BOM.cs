f9f5ec9ff1915c618edd867c6cb7f33c5d8ca2fb
JoeBuntu <jcooper@joepserver.net>
2011-10-30 14:33:35 -0400

    Implemented BOM entity

--- Inventory/Inventory.Core/Entities/BOM.cs
+++ Inventory/Inventory.Core/Entities/BOM.cs
@@ -7,5 +7,7 @@ namespace Inventory.Core.Entities
 {
     public class BOM : EntityBase
     {
+        public virtual Material Product { get; set; }
+        public virtual IList<Material> Components { get; set; }
     }
 }
