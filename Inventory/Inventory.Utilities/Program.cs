3da72662681c73b390d61872e7e19fc8b003bcbe
JoeBuntu <jcooper@joepserver.net>
2011-11-01 22:17:12 -0400

    Utilities - added crude main menu and option for exporting db schema

--- Inventory/Inventory.Utilities/Program.cs
+++ Inventory/Inventory.Utilities/Program.cs
@@ -2,13 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
 
 namespace Inventory.Utilities
 {
     class Program
     {
+        static Configuration m_NHConfiguration;
+        static ISessionFactory m_SessionFactory;
+
         static void Main(string[] args)
         {
+            log4net.Config.XmlConfigurator.Configure();
+            m_NHConfiguration = new ConfigurationBuilder().Build();
+            m_SessionFactory = m_NHConfiguration.BuildSessionFactory();
+            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+
+            Console.WriteLine("Choose Option:");
+            Console.WriteLine("  1. Export Schema");
+
+            ConsoleKey option = Console.ReadKey().Key;
+            switch (option)
+            {
+                case ConsoleKey.D1:
+                    CreateSchema();
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        static void CreateSchema()
+        {
+            SchemaExport export = new SchemaExport(m_NHConfiguration);
+            export.SetDelimiter(";");
+            export.SetOutputFile(@"db.sql").Execute(false, false, false);
+            Console.WriteLine("Schema Exported");
         }
     }
 }
