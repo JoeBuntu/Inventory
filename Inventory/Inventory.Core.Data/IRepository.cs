dbb01c271164b754493124551079b8ddb300f72c
JoeBuntu <jcooper@joepserver.net>
2011-11-02 22:26:42 -0400

    Inventory.Core.Data - added preliminary base repository classes

---
+++ Inventory/Inventory.Core.Data/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inventory.Core.Entities;
+
+namespace Inventory.Core.Data
+{
+    public interface IRepository<T> : IEnumerable<T> where T : EntityBase
+    {
+        void Add(T item);
+        bool Contains(T item);
+        int Count { get; }
+        bool Remove(T item);
+    }
+}
