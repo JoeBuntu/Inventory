d708335b1ca5349441ad6ed53c0a5e90e40ad154
JoeBuntu <jcooper@joepserver.net>
2011-11-09 18:21:16 -0500

    Web UI - added sorting to paging info

--- Inventory/Inventory.WebUI/Models/PagingInfo.cs
+++ Inventory/Inventory.WebUI/Models/PagingInfo.cs
@@ -7,12 +7,16 @@ namespace Inventory.WebUI.Models
 {
     public class PagingInfo
     {
-        public int TotalItems { get; set; }
-        public int ItemsPerPage { get; set; }
         public int CurrentPage { get; set; }
+        public int IsFirst { get { return CurrentPage = 1; } }
+        public int IsLast { get { return CurrentPage = TotalPages; } }
+        public int ItemsPerPage { get; set; }
+        public int TotalItems { get; set; }
         public int TotalPages
         {
             get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
         }
+        public string SortColumn { get; set; }
+        public bool SortAsc { get; set; }
     }
 }
