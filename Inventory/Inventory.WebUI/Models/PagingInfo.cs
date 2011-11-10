dad3fa217ca98f38b9f59922e0283779dc30870b
JoeBuntu <jcooper@joepserver.net>
2011-11-09 21:02:29 -0500

    Web UI - added group properties and other functionality to paging info view model

--- Inventory/Inventory.WebUI/Models/PagingInfo.cs
+++ Inventory/Inventory.WebUI/Models/PagingInfo.cs
@@ -7,15 +7,85 @@ namespace Inventory.WebUI.Models
 {
     public class PagingInfo
     {
-        public int CurrentPage { get; set; }
-        public int IsFirst { get { return CurrentPage = 1; } }
-        public int IsLast { get { return CurrentPage = TotalPages; } }
+        private int m_GroupSize = 5;
+
         public int ItemsPerPage { get; set; }
         public int TotalItems { get; set; }
         public int TotalPages
         {
             get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
         }
+
+        public int CurrentPage { get; set; }
+        public bool IsFirst { get { return CurrentPage == 1; } }
+        public bool IsLast { get { return CurrentPage == TotalPages; } }
+
+        public int GroupSize
+        {
+            get { return m_GroupSize; }
+            set
+            {
+                if (m_GroupSize % 2 > 0)
+                    throw new ArgumentException("Group Size not set to an odd number");
+                m_GroupSize = value;
+            }
+        }
+        public bool HasLeftGroupGap
+        {
+            get { return GroupStart > 1; }
+        }
+
+        public bool HasRightGroupGap
+        {
+            get { return GroupEnd < TotalPages; }
+        }
+
+        public int GroupStart
+        {
+            get
+            {
+                if (TotalPages > GroupSize)
+                {
+                    int sideCount = (int) ( GroupSize / 2);
+                    if (CurrentPage - sideCount > 1)
+                    {
+                        if (CurrentPage + sideCount < TotalPages)
+                        {
+                            return CurrentPage - sideCount;
+                        }
+                        else
+                        {
+                            return TotalPages - GroupSize + 1;
+                        }
+                    }
+                }
+                return 1;
+            }
+        }
+
+        public int GroupEnd
+        {
+            get
+            {
+                if (TotalPages > GroupSize)
+                {
+                    int sideCount = (int)(GroupSize / 2);
+                    if (CurrentPage + sideCount < TotalPages)
+                    {
+                        if (CurrentPage - sideCount > 1)
+                        {
+                            return CurrentPage + sideCount;
+                        }
+                        else
+                        {
+                            return GroupSize;
+                        }
+                    }
+                }
+                return TotalPages;
+            }
+        }
+
         public string SortColumn { get; set; }
         public bool SortAsc { get; set; }
     }
