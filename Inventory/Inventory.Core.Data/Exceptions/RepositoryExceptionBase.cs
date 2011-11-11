cce15b48558f561f4c1a984b292003bb2f071652
JoeBuntu <jcooper@joepserver.net>
2011-11-10 22:42:33 -0500

    Core.Data - created repository exception classes

---
+++ Inventory/Inventory.Core.Data/Exceptions/RepositoryExceptionBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inventory.Core.Data.Exceptions
+{
+    public class RepositoryExceptionBase : Exception
+    {
+    }
+}
