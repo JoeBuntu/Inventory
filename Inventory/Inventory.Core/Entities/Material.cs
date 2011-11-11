062893029bcc265ad63fd65c8e0ce197aa6abede
JoeBuntu <jcooper@joepserver.net>
2011-11-10 19:41:58 -0500

    Core - added validation attributes to Material

--- Inventory/Inventory.Core/Entities/Material.cs
+++ Inventory/Inventory.Core/Entities/Material.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace Inventory.Core.Entities
 {
     public class Material : EntityBase
     {
+        [Required(ErrorMessage = "Part Number is required", AllowEmptyStrings = false)]
+        [StringLength(30, ErrorMessage = "Part Number must not exceed 30 characters")]
         public virtual string PartNumber { get; set; }
+
+        [StringLength(50, ErrorMessage = "Description must not exceed 50 characters")]
         public virtual string Description { get; set; }
+
         public virtual MaterialType Type { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Pieces/Case must be greater than zero")]
         public virtual int PiecesPerCase { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Eaches/Case must be greater than zero")]
         public virtual int EachesPerPiece { get; set; }
     }
 }
