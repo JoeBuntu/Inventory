e1fabf0fdb46dee43aef89be9ac48ac032546ff8
JoeBuntu <jcooper@joepserver.net>
2011-11-12 18:59:07 -0500

    Core.Data - added repository for BOMs

---
+++ Inventory/Inventory.Core.Data/BOMsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using Inventory.Core.Entities;
+
+namespace Inventory.Core.Data
+{
+    public class BOMsRepository : Repository<BOM>, IBOMsRepository
+    {
+        public BOMsRepository(ISessionFactory sessionFactory)
+            : base(sessionFactory)
+        {
+        }
+    }
+}
---
+++ Inventory/Inventory.Core.Data/IBOMsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inventory.Core.Entities;
+
+namespace Inventory.Core.Data
+{
+    public interface IBOMsRepository : IRepository<BOM>
+    {
+    }
+}
