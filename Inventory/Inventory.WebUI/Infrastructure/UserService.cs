18b555ac945b200a637c164298109e00ca7dc82e
JoeBuntu <jcooper@joepserver.net>
2011-11-12 01:58:46 -0500

    Web UI - added user service

---
+++ Inventory/Inventory.WebUI/Infrastructure/UserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Inventory.Core.Entities;
+
+namespace Inventory.WebUI.Infrastructure
+{
+    public class UserService : IUserService
+    {
+        public User GetCurrentUser()
+        {
+            return new User() { Id = 2, Name = "grussell", Version = 1 };
+        }
+    }
+}
