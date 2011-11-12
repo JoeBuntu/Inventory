d9bc3ab24b31f5abd250c4cdb4733078b751f676
JoeBuntu <jcooper@joepserver.net>
2011-11-12 18:27:37 -0500

    Core - made changes to material validation and property cleaning

--- Inventory/Inventory.Core/Entities/Material.cs
+++ Inventory/Inventory.Core/Entities/Material.cs
@@ -8,19 +8,31 @@ namespace Inventory.Core.Entities
 {
     public class Material : EntityBase
     {
+        private string m_PartNumber;
+        private string m_Description;
+
         [Required(ErrorMessage = "Part Number is required", AllowEmptyStrings = false)]
         [StringLength(30, ErrorMessage = "Part Number must not exceed 30 characters")]
-        public virtual string PartNumber { get; set; }
+        [RegularExpression("(?i)^[A-Z0-9](.+)?", ErrorMessage = "Part Number must start with alpha-numeric character")]
+        public virtual string PartNumber
+        {
+            get { return m_PartNumber; }
+            set { m_PartNumber = value.Trim().ToUpper(); }
+        }
 
         [StringLength(50, ErrorMessage = "Description must not exceed 50 characters")]
-        public virtual string Description { get; set; }
-
+        public virtual string Description
+        {
+            get { return m_Description; }
+            set { m_Description = value.Trim().ToUpper(); }
+        }
+        
         public virtual MaterialType Type { get; set; }
 
         [Range(1, int.MaxValue, ErrorMessage = "Pieces/Case must be greater than zero")]
         public virtual int PiecesPerCase { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Eaches/Case must be greater than zero")]
+        [Range(1, int.MaxValue, ErrorMessage = "Eaches/Piece must be greater than zero")]
         public virtual int EachesPerPiece { get; set; }
     }
 }
