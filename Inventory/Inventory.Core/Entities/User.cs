b32baedae6ec43d82fab7ee66ddadcbc59d7696b
JoeBuntu <jcooper@joepserver.net>
2011-11-12 01:32:40 -0500

    Entities - created User

---
+++ Inventory/Inventory.Core/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Core.Entities
+{
+    public class User : EntityBase
+    {
+        [Required(ErrorMessage = "User name is required")]
+        [StringLength(25, MinimumLength = 1, ErrorMessage = "User name must be between 1 and 25 characters")]
+        public virtual string Name { get; }
+    }
+}
