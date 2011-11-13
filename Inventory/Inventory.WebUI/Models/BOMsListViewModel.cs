3a88bf18f0779f4a175a89fdf490caa73ac9a9fb
JoeBuntu <jcooper@joepserver.net>
2011-11-12 19:00:23 -0500

    Web UI - created view model for BOMs list

---
+++ Inventory/Inventory.WebUI/Models/BOMsListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Inventory.Core.Entities;
+
+namespace Inventory.WebUI.Models
+{
+    public class BOMsListViewModel
+    {
+        public IEnumerable<BOM> BOMs { get; set; }
+        public PagingInfoViewModel PagingInfo { get; set; }
+        public SortingInfoViewModel SortingInfo { get; set; }
+    }
+}
