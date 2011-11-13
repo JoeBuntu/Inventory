804aa5ac8f3588d6d1b09199be9da94a475a916e
JoeBuntu <jcooper@joepserver.net>
2011-11-12 19:00:57 -0500

    Web UI - created controller for BOMs

---
+++ Inventory/Inventory.WebUI/Controllers/BOMsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.Core.Data;
+using Inventory.Core.Entities;
+using Inventory.WebUI.Infrastructure;
+
+namespace Inventory.WebUI.Controllers
+{
+    public class BOMsController : Controller
+    {
+        private readonly IBOMsRepository m_Repository;
+        private readonly IUserService m_UserService;
+        private readonly int m_PageSize = 25;
+
+        public BOMsController(IBOMsRepository repository, UserService user_service)
+        {
+            m_Repository = repository;
+            m_UserService = user_service;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<BOM> boms = m_Repository.Get(0, m_PageSize);
+            return View(boms);
+        }
+
+    }
+}
